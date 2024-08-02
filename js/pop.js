bfe78a5a2109d9da956bf165fa23a1135cd5dfcc
Gabriel <2016069@crosemont.qc.ca>
2024-08-02 12:49:58 -0400

    Envoi Email Non fonctionnelle ver et pop js

--- js/pop.js
+++ js/pop.js
@@ -1,19 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var openButton = document.getElementsByClassName("openModal");
-    var closeButton = document.getElementById("closeModal");
-    var modal = document.getElementById("modal");
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all the buttons that open modals
+    var modalButtons = document.querySelectorAll(".openModal");
 
-    openButton.addEventListener("click", () => {
-        modal.style.display = "block";
+    // Get all the close buttons
+    var closeButtons = document.querySelectorAll(".close");
+
+    // Add click event to each button
+    modalButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var modalId = button.getAttribute("data-modal");
+            var modal = document.getElementById(modalId);
+            modal.style.display = "block";
+        });
     });
 
-    closeButton.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
-
-    window.addEventListener("click", (event) => {
-        if (event.target === modal) {
+    // Add click event to each close button
+    closeButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var modalId = button.getAttribute("data-modal");
+            var modal = document.getElementById(modalId);
             modal.style.display = "none";
-        }
+        });
     });
+
+    // Close modal when clicking outside of modal content
+    window.onclick = function(event) {
+        if (event.target.classList.contains("modal")) {
+            event.target.style.display = "none";
+        }
+    };
 });
