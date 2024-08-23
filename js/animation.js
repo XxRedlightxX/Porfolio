793a4e7d911ca697974ee9c8261a6bba5ed9c800
unknown <XxRedlightxX>
2024-08-23 16:22:56 -0400

    Responsive design pour petit ecran

--- js/animation.js
+++ js/animation.js
@@ -3,11 +3,11 @@ ScrollReveal({ reset:
     true,
     distance:'20px',
     duration : 2500,
-    delay : 150
+    delay : 100
 
 });
 
-ScrollReveal().reveal('.hero-text h1', {delay: 500, origin: "left"});
+ScrollReveal().reveal('.hero-text h1', {delay: 500, origin: "right"});
 ScrollReveal().reveal('.service', { interval: 200,origin: "left" });
 ScrollReveal().reveal('.Contact-gauche', { interval: 200,origin: "right" });
 
