b166c0bdfb8046e94caa08c1a73f3edf1b1b8e08
unknown <XxRedlightxX>
2024-08-30 23:17:05 -0400

    fonctionalite envoi email

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 
-ScrollReveal({ reset: 
+/*ScrollReveal({ reset: 
     true,
     distance:'20px',
     duration : 2500,
@@ -9,7 +9,7 @@ ScrollReveal({ reset:
 
 ScrollReveal().reveal('.hero-text h1', {delay: 500, origin: "right"});
 ScrollReveal().reveal('.service', { interval: 200,origin: "left" });
-ScrollReveal().reveal('.Contact-gauche', { interval: 200,origin: "right" });
+ScrollReveal().reveal('.Contact-gauche', { interval: 200,origin: "right" });*/
 
 
 
